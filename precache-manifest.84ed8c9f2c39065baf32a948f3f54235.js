86921db1bcd7e84786f0d07afdd7d30c1028f90f
jian <hjxboy@gmail.com>
2023-02-03 19:23:40 -0800

    v2

---
+++ precache-manifest.84ed8c9f2c39065baf32a948f3f54235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec7ac879a2c47aef92b70ffbfeb29e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "858c86feecff91460f1d",
+    "url": "/static/css/main.5a032b4e.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858c86feecff91460f1d",
+    "url": "/static/js/main.11fec3bd.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
